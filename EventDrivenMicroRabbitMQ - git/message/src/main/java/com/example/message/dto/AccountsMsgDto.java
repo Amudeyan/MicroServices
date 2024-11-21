5b2932a643c40959ce8f40ce6c079ad6e7e7954c
Amudeyan <aravinthjetbrain@gmail.com>
2024-11-21 10:50:26 +0530

    Delete EventDrivenMicroRabbitMQ - git directory
    uploading another remote dir

--- EventDrivenMicroRabbitMQ - git/message/src/main/java/com/example/message/dto/AccountsMsgDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.message.dto;
-
-import org.springframework.stereotype.Component;
-
-
-public record AccountsMsgDto(Long accountNumber,String name,String email,String mobileNumber) {
-}
